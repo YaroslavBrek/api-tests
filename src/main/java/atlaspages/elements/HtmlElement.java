8e681dacdbc14d547cb01ba59723d9a945e68d52
Iaroslav Brek <iaroslav.brek@nixs.com>
2023-02-14 14:35:20 +0100

    - Delete unused files

--- src/main/java/atlaspages/elements/HtmlElement.java
+++
@@ -1,6 +0,0 @@
-package atlaspages.elements;
-
-import io.qameta.atlas.webdriver.AtlasWebElement;
-
-public interface HtmlElement extends AtlasWebElement<HtmlElement> {
-}
--- src/main/java/atlaspages/pages/HomePage.java
+++
@@ -1,8 +0,0 @@
-package atlaspages.pages;
-
-import atlaspages.layout.WithHeader;
-import io.qameta.atlas.webdriver.WebPage;
-
-public interface HomePage extends WebPage, WithHeader {
-
-}
