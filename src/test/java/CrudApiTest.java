df19df5a4f753bb24d78961c68d6820b4d25f2fd
Iaroslav Brek <iaroslav.brek@nixs.com>
2023-02-14 14:37:54 +0100

    - Update tests

--- src/test/java/CrudApiTest.java
+++ src/test/java/CrudApiTest.java
@@ -1,43 +1,48 @@
-import static io.restassured.RestAssured.given;
 import static org.apache.hc.core5.http.HttpStatus.SC_NOT_FOUND;
 import static org.apache.hc.core5.http.HttpStatus.SC_OK;
 
+import api.UserApiSteps;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.http.ContentType;
 
 public class CrudApiTest extends Assert {
+    private UserApiSteps userApiSteps;
 
-  @Test(groups = "api")
-  public void getUsers() {
-    given()
-        .when()
-        .get("http://crud:9000/user/")
-        .then()
-        .assertThat()
-        .statusCode(200)
-        .contentType(ContentType.JSON);
-  }
+    public UserApiSteps onUserApi() {
+        if (userApiSteps == null) {
+            userApiSteps = new UserApiSteps();
+        }
+        return userApiSteps;
+    }
 
-  @Test(groups = "api")
-  public void getSingleUser() {
-    given()
-        .when()
-        .get("http://crud:9000/user/2")
-        .then()
-        .assertThat()
-        .statusCode(SC_OK)
-        .contentType(ContentType.JSON);
-  }
+    @Test(groups = "api")
+    public void getUsers() {
+        onUserApi()
+                .sendGetAllUsers()
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+    }
 
-  @Test(groups = "api")
-  public void getNotFoundError() {
-    given()
-        .when()
-        .get("http://crud:9000/user/666")
-        .then()
-        .assertThat()
-        .statusCode(SC_NOT_FOUND);
-  }
+    @Test(groups = "api")
+    public void getSingleUser() {
+        onUserApi()
+                .sendGetUser("2")
+                .then()
+                .assertThat()
+                .statusCode(SC_OK)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test(groups = "api")
+    public void getNotFoundError() {
+        onUserApi()
+                .sendGetUser("666")
+                .then()
+                .assertThat()
+                .statusCode(SC_NOT_FOUND);
+    }
 }
